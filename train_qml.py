f75717e7054e0c630b03f3514449502767d92fad
obsqyr <qns.sepp@gmail.com>
2021-02-11 17:15:32 +0100

    Implemented simple machine for predicting energy from positions, using CM representation.

--- train_qml.py
+++ train_qml.py
@@ -4,6 +4,7 @@ from qml.kernels import gaussian_kernel
 from tutorial_data import energy_pbe0
 from qml.math import cho_solve
 import numpy as np
+from qml.representations import *
 
 import vasp_parser as vp
 
@@ -12,20 +13,53 @@ from maml.apps.pes import MTPotential
 
 def train(tr, target):
     print("training")
+    # do I want to convert the training data into some representation
+    
+    positions = []
+    for i in range(100):
+        positions.append(tr[:,i])
+        
 
+    representations = []
+    nuclear_charge = np.array([13]*32)
+    for pos in positions:
+        representations.append(generate_coulomb_matrix(nuclear_charge, pos, size=32, sorting="row-norm"))
+
+    X = np.array(representations)
+
+    X_train = X[:50]
+    X_test = X[50:]
+    Y_train = target[:50]
+    Y_test = target[50:]
+
+    # start with training one machine (proof of concept) for one atom
     sigma = 4000
-    K = gaussian_kernel(tr, tr, sigma)
+    K = gaussian_kernel(X_train, X_train, sigma)
+    #print(K)
+    #print(tr[0])
 
     # Add a small lambda to the diagonal of the kernel matrix
     K[np.diag_indices_from(K)] += 1e-8
 
-    print(len(K), len(target))
+    #print(len(K), len(target[0]))
 
     # Use the built-in Cholesky-decomposition to solve
-    alpha = cho_solve(K, target) 
+    alpha = cho_solve(K, Y_train) 
 
-    print("Alphas:")
-    print(alpha)
+    #print("Alphas:")
+    #print(alpha)
+
+    return alpha, sigma, X_train, X_test, Y_test
+
+def evaluate(alpha, sigma, X_train, X_test, Y_test):
+    print("evaluating")
+    Ks = gaussian_kernel(X_test, X_train, sigma)
+
+    Y_pred = np.dot(Ks, alpha)
+
+    print("MAE:")
+    print(np.mean(np.abs(Y_pred - Y_test)))
+
     
 def train_qml_regressor():
     print("training QML regressor")
@@ -49,13 +83,14 @@ def train_qml_regressor():
     sigma = 4000
     K = gaussian_kernel(X_training, X_training, sigma)
     #print("Gaussian kernel:")
+    #print(K)
     
     # Add a small lambda to the diagonal of the kernel matrix
     K[np.diag_indices_from(K)] += 1e-8
 
     # Use the built-in Cholesky-decomposition to solve
     alpha = cho_solve(K, Y_training) 
-    #print(len(K), len(Y_training))
+    print(len(K), len(Y_training))
     print(Y_training)
 
     #print("Alphas:")
@@ -108,15 +143,15 @@ def divide_data(data, num_atoms):
 
 if __name__ == "__main__":
     # import data from infiles
-    forces, positions, potentials = vp.read_infiles()
+    forces, positions, potentials, num_atoms = vp.read_infiles('Al_300K/')
 
     # amount of timesteps is equal to length of potentials
     timesteps = len(potentials)
-    print(timesteps)
+    #print(timesteps)
 
     # divide forces and positions into matrices for each atom
-    forces = divide_data(forces, 32)
-    positions = divide_data(positions, 32)
+    forces = divide_data(forces, num_atoms)
+    positions = divide_data(positions, num_atoms)
     
     # divide into training and test data
     train_f = forces[:,0:50]
@@ -128,8 +163,7 @@ if __name__ == "__main__":
     train_pot = potentials[:50]
     test_pot = potentials[50:]
 
-    #print(train_pot)
-    #print(test_pot)
-    
+    #print(train_f)
     #train_qml_regressor()
-    #train(train_pos, train_f)
+    alpha, sigma, X_train, X_test, Y_test = train(forces, potentials)
+    evaluate(alpha, sigma, X_train, X_test, Y_test)
