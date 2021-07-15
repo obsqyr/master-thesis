317d17d53c1be4bb5528d542e5f69b2329b7ee58
obsqyr <qns.sepp@gmail.com>
2021-07-15 12:31:45 +0200

    Added plot sh illustrating the error function

--- ml_example.py
+++ ml_example.py
@@ -7,7 +7,7 @@ import random
 if __name__ == "__main__":
     print("ML example")
     # set font size
-    font = {'size'   : 14}
+    font = {'size'   : 16}
 
     plt.rc('font', **font)
     
@@ -28,8 +28,8 @@ if __name__ == "__main__":
     tr_data = data[:15]
     ts_data = data[15:]
 
-    plt.plot(time, sine, color='black', linewidth=4)
-    plt.scatter(time, noisy_sine, s=70)
+    plt.plot(time, sine, color='black', linewidth=5)
+    plt.scatter(time, noisy_sine, s=90)
 
     plt.legend(["y=sin(x)", "Complete data set"])
     plt.savefig('figures/ML/pure.png')
@@ -52,8 +52,26 @@ if __name__ == "__main__":
     # polynomial regression
     # get training data
     x,y = zip(*tr_data)
-    
     model_line = np.arange(0,6,0.001)
+
+    # make error function plot
+    figure(num=None, figsize=(7, 5), dpi=80, facecolor='w', edgecolor='k')
+    plt.title("")
+    plt.xlabel("")
+    plt.ylabel("")
+    #plt.xscale('log')
+    plt.grid(True)
+    plt.ylim([-1.5, 1.5])
+
+    x_ts,y_ts = zip(*ts_data)
+    
+    model = np.poly1d(np.polyfit(x,y,1)) 
+    plt.plot(model_line, model(model_line), linestyle='--', linewidth=3)
+    plt.scatter(x_ts[:3], y_ts[:3], color='green', s=70)
+    
+    plt.legend(["Model", "Testing data"])
+    plt.savefig('figures/ML/error_function.png')
+
     for i in range(1,10):
         figure(num=None, figsize=(7, 5), dpi=80, facecolor='w', edgecolor='k')
         plt.title("")
